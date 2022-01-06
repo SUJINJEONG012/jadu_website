b0aee31c495847a67ad52f4e18d0de1cedebc0ba
SUJINJEONG012 <peekaboo32@naver.com>
2022-01-06 17:57:59 +0900

    전체메인수정

--- js/main_ba.js
+++ js/main_ba.js
@@ -1,33 +1,55 @@
-const spans = document.querySelectorAll('span');
-const contents = document.querySelectorAll('.content');
-const firstTop = contents[0].offsetTop;
-const secondTop = contents[1].offsetTop;
-const thirdTop = contents[2].offsetTop;
-const fourTop = contents[3].offsetTop;
-const fifthTop = contents[4].offsetTop;
+const menus = document.querySelectorAll('li');
+const contents = document.querySelectorAll('.contents');
 
-spans[0].onclick = function(){
-    window.scroll({top:firstTop, behavior:'smooth'});
+//사이트맵 열고닫기 상수 지정
+const html = document.querySelector('html');
+const header = html.querySelector('.header');
+const btnSitemap = html.querySelector('.navBtntoggle');
+const sitemap = html.querySelector('.nav-mobile');
+
+
+//toggle 클릭했을 경우 열고 닫히는 이벤트 함수 
+function toggleClick(){
+  html.classList.toggle('active'); // overflow: hidden;
+  btnSitemap.classList.toggle('active');
+  sitemap.classList.toggle('active');
 }
 
-spans[1].onclick = function(){
-    window.scroll({top:secondTop, behavior:'smooth'});
+//함수실행
+	btnSitemap.addEventListener('click', () => {
+   toggleClick();
+	});
+
+
+
+window.onload = function(){
+  for(let i =0; i<menus.length; i++){
+    menus[i].setAttribute("onclick","elemRemove("+i+")")
+  }
 }
 
-spans[2].onclick = function(){
-    window.scroll({top:thirdTop, behavior:'smooth'});
+function elemRemove(menuNum){
+  for(let i=0 ; i<menus.length; i++){
+    if(i === menuNum % 5){
+      window.scroll({top:contents[i].offsetTop, behavior:'smooth'});
+      document.getElementById("menu"+(i+1)).classList.add('active');
+       
+      if (matchMedia("screen and (max-width: 1024px)").matches) { 
+        toggleClick();    
+      } 
+      
+    }else{
+      document.getElementById("menu"+(i+1)).classList.remove('active');
+    }
+  }
 }
 
-spans[3].onclick = function(){
-    window.scroll({top:fourTop, behavior:'smooth'});
-}
-
-spans[4].onclick = function(){
-  window.scroll({top:fifthTop, behavior:'smooth'});
-}
+// 함수호출 toggleClick(); 를 함수로 만든 후, 햄버게메뉴 클릭시 열고/닫히는 이벤트와
+// 모바일 메뉴클릭시 콘텐츠로 변경될때 햄버거메뉴가 닫히는 이벤트랑 동일 한번에 함수로 만듦  
 
 
 
+//부드럽게 내려오는 
 
 function isElementUnderBottom(elem, triggerDiff) {
     const { top } = elem.getBoundingClientRect();
@@ -51,3 +73,10 @@ function isElementUnderBottom(elem, triggerDiff) {
   }
   
   window.addEventListener('scroll', handleScroll);
+
+
+
+
+
+
+	
