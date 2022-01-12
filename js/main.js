6ee9106d665c574645f5a4536c9fd003b7423079
SUJINJEONG012 <peekaboo32@naver.com>
2022-01-12 15:08:06 +0900

    모바일 css 수정

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@ function handleScroll() {
     elems.forEach(elem => {
         if (isElementUnderBottom(elem, -20)) {
             elem.style.opacity = "0";
-            elem.style.transform = 'translateY(70px)';
+            elem.style.transform = 'translateY(10px)';
 
         } else {
             elem.style.opacity = "1";
