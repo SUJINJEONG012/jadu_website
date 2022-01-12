0d47a7f2d1b60d4857336588c50bad3c4ea704bd
SUJINJEONG012 <peekaboo32@naver.com>
2022-01-12 10:07:50 +0900

    모바일 슬라이드 추가

--- js/main.js
+++ js/main.js
@@ -52,8 +52,6 @@ function elemRemove(menuNum){
 
 //부드럽게 내려오는 
 
-//부드럽게 내려오는 
-
 function isElementUnderBottom(elem, triggerDiff) {
     const { top } = elem.getBoundingClientRect();
     const { innerHeight } = window;
@@ -82,104 +80,72 @@ window.addEventListener('scroll', handleScroll);
 
 
 
-	// 브릭스란 ? 슬라이드
-	const slideList = document.querySelector('ul.slide_list');
-	const slideBlock = document.querySelectorAll('.slide_block');
-	const slideLen = slideBlock.length; // li 길이
-	const slideBlockWidth = 100 / (slideLen + 1); // li 너비 계산
-	const btnSlide = document.querySelectorAll('.btn_slide');
-	
-    let curIndex = 0;
-	let curBtn = btnSlide[curIndex];
-	curBtn.classList.add('active');
+	// 브릭스이미지 슬라이드
 
-	slideList.style.width = `${100 * (slideLen + 1)}%`; // ul 너비 지정
-	slideBlock.forEach((element) => {
-		element.style.width = `${slideBlockWidth}%`;
-	}); // li 너비 지정
+	var slides = document.getElementsByClassName('slides');
+var dots =  document.getElementsByClassName('dots');
 
-	// 마지막 슬라이드 뒤에 첫번째 슬라이드 붙이기
-	const slideBlockFirst = slideList.firstElementChild.cloneNode(true);
-	slideList.appendChild(slideBlockFirst);
+var slideIndex = 0;
 
-	// 페이지 로드되면 자동재생
-	let setting; // clear를 위한 전역함수 선언
+document.getElementById('allSlide').innerHTML = slides.length;
 
-	function autoPlay() {
-		setting = setInterval(() => {
-			slideList.style.transition = '.5s';
-			slideList.style.transform = `translateX(-${slideBlockWidth * (curIndex + 1)}%`;
-			
-			curBtn.classList.remove('active');
-			curBtn = btnSlide[++curIndex];				
-			
-			if(curIndex === slideLen) {
-				setTimeout(() => {
-					slideList.style.transition = '0s';
-					slideList.style.transform = 'translateX(0)';
-				}, 501);
-				curIndex = 0;
-				curBtn = btnSlide[0];
-			};
 
-			curBtn.classList.add('active');
-		}, 5000);
-	};
 
-	document.addEventListener("DOMContentLoaded", () => {
-		autoPlay();
-	});
+var myVar;
 
-	// 자동재생 정지 & 다시재생
-	const btnAutoPause = document.querySelector('.btn_auto.pause');
-	const btnAutoPlay = document.querySelector('.btn_auto.play');
 
-	btnAutoPause.addEventListener('click', () => {
-		btnAutoPause.style.display = 'none';
-		btnAutoPlay.style.display = 'block';
-		clearInterval(setting);
-	});
+showSlides();
 
-	btnAutoPlay.addEventListener('click', () => {
-		btnAutoPause.style.display = 'block';
-		btnAutoPlay.style.display = 'none';
-		autoPlay();
-	});
+function showSlides(){
+    for(var i=0; i<slides.length; i++){
+        slides[i].style.display = 'none';
+        dots[i].className = dots[i].className.replace(" on", "");}
+    
+    slideIndex++;
 
-	// 버튼 클릭시 슬라이드 이동
-	for (let i = 0; i < slideLen; i++){
-		btnSlide[i].onclick = () => {
-			if ( btnAutoPlay.style.display === 'block' ) {
-				slideList.style.transition = '.5s';
-				slideList.style.transform = `translateX(-${slideBlockWidth * i}%`; // 클릭한 슬라이드로 이동
-			
-				clearInterval(setting); // 자동재생 중지
+    if(slideIndex > slides.length){slideIndex = 1;}
 
-				for (let i = 0; i < slideLen; i++) {
-					btnSlide[i].classList.remove('active');
-				}; // active 전체 제거
+    document.getElementById('thisSlide').innerHTML = slideIndex;
+    slides[slideIndex-1].style.display = 'block';
+    dots[slideIndex-1].className += ' on';
+    
+    myVar = setTimeout(showSlides, 5000);
+}//showSlides
 
-				btnSlide[i].classList.add('active'); // 클릭한 곳 active 추가
+function stopSlide(){ clearTimeout(myVar);} //stopSlide
 
-				curIndex = i; // 흐름이 이어지도록 curIndex 설정
-			} // 자동재생이 멈춰있을 경우
+/* ========= */
 
-			else {
-				slideList.style.transition = '.5s';
-				slideList.style.transform = `translateX(-${slideBlockWidth * i}%`; // 클릭한 슬라이드로 이동
-				
-				clearInterval(setting); // 자동재생 중지
-				curBtn.classList.remove('active'); // 자동재생되던 곳 active 제거
-				btnSlide[i].classList.add('active'); // 클릭한 곳 active 추가
-				
-				curIndex = i; // 흐름이 이어지도록 curIndex 설정
-				
-				setTimeout(() => {
-					btnSlide[i].classList.remove('active'); 
-				},5000); // 3초 뒤 클릭한 곳 active 제거
-				
-				autoPlay(); // 자동재생 다시 실행
-			} // 자동재생이 실행중일 경우	
-		}
-	};
+function prevNext(num){
+    stopSlide();
+    slideIndex += num;
+
+    for(var i=0; i<slides.length; i++){
+        slides[i].style.display = 'none';
+        dots[i].className = dots[i].className.replace(" on", ""); }
+
+    if(slideIndex < 1){ slideIndex = slides.length; }
+    if(slideIndex > slides.length ){ slideIndex = 1; }
+
+    document.getElementById('thisSlide').innerHTML = slideIndex;
+    slides[slideIndex - 1 ].style.display = 'block';
+    dots[slideIndex - 1 ].className += ' on';
+}
+
+/* ========= */
+function currentSlide(num){
+    stopSlide();
+    slideIndex = num;
+
+    for(var i=0; i<slides.length; i++){
+        slides[i].style.display = 'none';
+        dots[i].className = dots[i].className.replace(" on", ""); }
+    
+    document.getElementById('thisSlide').innerHTML = slideIndex;
+    slides[slideIndex - 1 ].style.display = 'block';
+    dots[slideIndex - 1 ].className += ' on';
+
+}
+
+
 
