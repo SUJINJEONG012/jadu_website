d9aed9f7b168fe3ec21f119cd59a4f81738a48d2
SUJINJEONG012 <peekaboo32@naver.com>
2022-01-17 14:06:11 +0900

    모바일 수정

---
+++ js/test.js
@@ -0,0 +1,36 @@
+let x = 'hello world';
+console.log(x);
+
+//객체
+const person = {
+    firstName :'John',
+    lastName : 'Don',
+    age :50,
+    eyeColor:'bule'
+};
+
+console.log(person);
+person['age'] = 52;
+person.age =52;
+
+
+console.log(person);
+
+
+let age = 12;
+let busFare = 0; //버스기본요금
+
+// if( age ==){
+    
+// }else if(){
+
+// }else if(){
+    
+// }else if(){
+    
+// }else if(){
+    
+// }else{
+
+// }
+
