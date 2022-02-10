518e8ee3c36a2c1caed20e8a7e2913400db40a1e
alxsm90@naver.com <alxsm90@naver.com>
2022-02-10 19:37:39 +0900

    카테고리 분리

--- js/donation.js
+++ js/donation.js
@@ -2,7 +2,7 @@
 
 
 
-
+// 모바일 드롭다운 메뉴
 $(document).ready(function  () {
 	$(".location-inner > ul > li > a").click(function  () {
 		$(this).siblings().slideToggle();
@@ -13,3 +13,22 @@ $(document).ready(function  () {
 	});
 });
 
+
+// 탭메뉴
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    console.log(target);
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    });
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    });
+    target.classList.add('active')
+    tab.classList.add('active')
+  })
+})
